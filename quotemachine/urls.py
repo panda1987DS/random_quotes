4e9554425cc4322981e992e36e987a479a681e97
panda <pankratova.dasha@gmail.com>
2025-07-01 13:50:58 +0100

    Add like and dislike

--- quotemachine/urls.py
+++ quotemachine/urls.py
@@ -5,5 +5,7 @@ from quotes import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('add/', views.add_quote, name='add_quote'),
+    path('like/<int:quote_id>/', views.like_quote, name='like_quote'),
+    path('dislike/<int:quote_id>/', views.dislike_quote, name='dislike_quote'),
     path('', views.random_quote, name='random_quote')
 ]
--- quotes/views.py
+++ quotes/views.py
@@ -2,7 +2,8 @@ import random
 from django.shortcuts import render, redirect
 from .models import Quote
 from .forms import QuoteForm
-
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
 
 def random_quote(request):
     quotes = Quote.objects.all()
@@ -24,3 +25,17 @@ def add_quote(request):
     else:
         form = QuoteForm()
     return render(request, 'add.html', {'add': form})
+
+@require_POST
+def like_quote(request, quote_id):
+    quote = Quote.objects.get(id=quote_id)
+    quote.likes += 1
+    quote.save()
+    return JsonResponse({'likes': quote.likes})
+
+@require_POST
+def dislike_quote(request, quote_id):
+    quote = Quote.objects.get(id=quote_id)
+    quote.dislikes += 1
+    quote.save()
+    return JsonResponse({'dislikes': quote.dislikes})
