72624cdd04436264202bc48ee5ccaf31dc4c508d
panda <pankratova.dasha@gmail.com>
2025-07-02 09:25:14 +0100

    Tests have been reworked

--- quotes/tests.py
+++ quotes/tests.py
@@ -1,20 +1,30 @@
 from django.test import TestCase
+
+from quotes.forms import QuoteForm
 from quotes.models import Quote
 
 
 class QuoteTests(TestCase):
-    def test_add_unique_quote(self):
-        Quote.objects.create(text="text", source="source", weight=1)
-        self.assertEqual(Quote.objects.count(), 1)
+    def setUp(self):
+        Quote.objects.create(text="0", source="1", weight=1)
 
     def test_duplicate_quote_not_allowed(self):
-        Quote.objects.create(text="text", source="source", weight=1)
-        with self.assertRaises(Exception):
-            Quote.objects.create(text="text", source="source", weight=2)
+        data = {
+            "text": "0",
+            "source": "1",
+            "weight": 2
+        }
+        form = QuoteForm(data=data)
+        self.assertFalse(form.is_valid())
+        self.assertIn("__all__", form.errors)
+        self.assertIn("Такая цитата уже существует", form.errors["__all__"][0])
 
     def test_source_limit(self):
-        Quote.objects.create(text="1", source="source", weight=1)
-        Quote.objects.create(text="2", source="source", weight=1)
-        Quote.objects.create(text="3", source="source", weight=1)
-        with self.assertRaises(Exception):
-            Quote.objects.create(text="4", source="source", weight=1)
+        Quote.objects.create(text="1", source="1", weight=1)
+        Quote.objects.create(text="2", source="1", weight=2)
+        Quote.objects.create(text="3", source="1", weight=3)
+
+        data = {"text": "4", "source": "1", "weight": 4}
+        form = QuoteForm(data=data)
+        self.assertFalse(form.is_valid())
+        self.assertIn("Уже есть 3 цитаты из этого источника.", form.errors["__all__"][0])
